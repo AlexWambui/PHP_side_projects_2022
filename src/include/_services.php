8a6abe0b43a61108614f18f342ed497d7440a393
AlexWambui <alexwambuik@gmail.com>
2022-06-17 22:59:06 +0300

    handles bookings and services

--- src/include/_services.php
+++ src/include/_services.php
@@ -43,3 +43,37 @@ function delete_service()
     setcookie("success", "Service has been deleted 😮.", time() + 2);
     header('location: ./services.php');
 }
+
+function book_service()
+{
+    global $db_connection;
+    $customer_id = $_SESSION['user_id'];
+    $service_id = $_REQUEST['service_id'];
+    $date_of_request = $_REQUEST['date_of_request'];
+    $venue_address = $_REQUEST['venue_address'];
+    $additional_information = $_REQUEST['additional_information'];
+
+    $sql = mysqli_prepare($db_connection, "INSERT INTO bookings (`customer_id`, `service_id`, `date_of_request`, `venue_address`, `additional_information`) VALUES (?,?,?,?,?)");
+    mysqli_stmt_bind_param($sql, "iisss", $customer_id, $service_id, $date_of_request, $venue_address, $additional_information);
+    mysqli_stmt_execute($sql) or die(mysqli_error($db_connection));
+    setcookie('success', 'Booked!', time() + 2);
+    header('location: ./bookings.php');
+}
+
+function fetch_user_bookings(): mysqli_result|bool
+{
+    global $db_connection;
+    $id = $_SESSION['user_id'];
+
+    $fetched_records = $db_connection->query(
+        "SELECT users.id AS user_id, users.*, services.id AS service_id, services.*, bookings.*
+        FROM bookings
+        LEFT JOIN services
+        ON services.id = bookings.service_id
+        LEFT JOIN users
+        ON users.id = bookings.customer_id 
+        WHERE users.id = '$id' ") 
+        or die($db_connection
+    );
+    return $fetched_records;
+}
