bfed29c2db60992b9f2aa2df6bb45e1369b693c4
AlexWambui <alexwambuik@gmail.com>
2022-10-05 03:44:35 +0300

    home page

---
+++ Wanderis_Animal_Feed_MS/index.php
@@ -0,0 +1,48 @@
+<?php
+include_once "src/include/functions.php"
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta 
+        name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link rel="stylesheet" href="assets/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <title><?= $app_full_name ?></title>
+</head>
+<body>
+    <section class="Home">
+        <div class="home_navbar">
+            <div class="navbar">
+                <div class="logo">
+                    <p><?= $app_name ?></p>
+                </div>
+                <div class="nav_links">
+                    <ul>
+                        <li><a href="src/login.php">Login</a></li>
+                        <li><a href="src/signup.php">Signup</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="hero_section">
+            <div class="app_details">
+                <h1><?= $app_full_name ?></h1>
+                <h2><?= $app_slogan ?></h2>
+            </div>
+        </div>
+        <div class="footer">
+            <p>copyright&copy;2022. All right reserved.</p>
+        </div>
+    </section>
+</body>
+</html>
