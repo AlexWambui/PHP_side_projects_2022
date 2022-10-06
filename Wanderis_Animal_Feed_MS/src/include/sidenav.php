14515e902ca1d54c7201c06fb5019230ffe94ecf
AlexWambui <alexwambuik@gmail.com>
2022-10-06 05:20:34 +0300

    added the link to the reports page

--- Wanderis_Animal_Feed_MS/src/include/sidenav.php
+++ Wanderis_Animal_Feed_MS/src/include/sidenav.php
@@ -4,12 +4,14 @@
     </div>
     <ul>
         <li><a href="dashboard.php"><span class="icon-home"></span> Dashboard</a></li>
-        <?php if ($_SESSION['user_level'] == 3): ?>
+        <?php if ($_SESSION['user_level'] != 1): ?>
             <li><a href="users.php"><span class="icon-users"></span> Users</a></li>
         <?php endif; ?>
         <li><a href="products.php"><span class="icon-shopping-cart"></span> Products</a></li>
-        <li><a href="sales.php"><span class="icon-money"></span> Sales</a></li>
-        <li><a href="reports.php"><span class="icon-file"></span> Reports</a></li>
+        <?php if ($_SESSION['user_level'] != 1): ?>
+            <li><a href="sales.php"><span class="icon-money"></span> Sales</a></li>
+            <li><a href="reports.php"><span class="icon-file"></span> Reports</a></li>
+        <?php endif; ?>
     </ul>
     <div class="account_details">
         <ul>
