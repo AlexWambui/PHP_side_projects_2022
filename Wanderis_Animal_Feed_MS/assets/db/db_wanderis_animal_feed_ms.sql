7e22fe426eda3f002f5eeb96761626d6c66d06ac
AlexWambui <alexwambuik@gmail.com>
2022-10-10 16:55:10 +0300

    updated db with more data

--- Wanderis_Animal_Feed_MS/assets/db/db_wanderis_animal_feed_ms.sql
+++ Wanderis_Animal_Feed_MS/assets/db/db_wanderis_animal_feed_ms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2022 at 04:22 AM
+-- Generation Time: Oct 10, 2022 at 03:54 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -41,8 +41,19 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category`, `product_name`, `price`, `product_image`, `date_created`) VALUES
-(4, 1, 'Chicken Mash.', 1200, 'uploads/65238chicken mash.jpg', '2022-10-05 16:45:04'),
-(5, 3, 'Josera Salmon & Potato.', 1500, 'uploads/23588download.jpg', '2022-10-05 16:45:47');
+(4, 1, 'Chicken Mash.', 1000, 'uploads/65238chicken mash.jpg', '2022-10-05 16:45:04'),
+(5, 3, 'Josera Salmon & Potato.', 1500, 'uploads/23588download.jpg', '2022-10-05 16:45:47'),
+(7, 4, 'Fish fingers.', 900, 'uploads/77367download (3).jpg', '2022-10-06 07:01:06'),
+(8, 2, 'Pasupati Milk Concentrate.', 1350, 'uploads/51706Milk-Concentrate.jpg', '2022-10-10 12:55:52'),
+(9, 4, 'Friskies Gravy Swiriers.', 1400, 'uploads/90538download (3).jpg', '2022-10-10 12:57:01'),
+(10, 1, 'Chick Starter Grower', 1100, 'uploads/37509download (2).jpg', '2022-10-10 12:57:51'),
+(11, 4, 'Meow Mix', 1500, 'uploads/23142download (1).jpg', '2022-10-10 12:58:15'),
+(12, 2, 'Agrofeed Super Mix.', 1150, 'uploads/25525agrofeed super mix.jpg', '2022-10-10 13:09:45'),
+(13, 2, 'Producer\'s Pride Cattle Cubes.', 1560, 'uploads/19679Cattle Cubes.jpg', '2022-10-10 13:10:34'),
+(14, 2, 'Nutri Rich', 1650, 'uploads/44172Nutri Rich.jpg', '2022-10-10 13:10:54'),
+(15, 1, 'Egg Makers 15 Pellets', 1850, 'uploads/80689egg maker.jpg', '2022-10-10 13:13:15'),
+(16, 1, 'Layers Mash', 1230, 'uploads/73835layers mash.jpg', '2022-10-10 13:14:36'),
+(17, 1, 'Starter Crumbles', 1480, 'uploads/64852Starter Crumbles.jpg', '2022-10-10 13:16:05');
 
 -- --------------------------------------------------------
 
@@ -65,7 +76,9 @@ CREATE TABLE `sales` (
 
 INSERT INTO `sales` (`id`, `product_id`, `amount_in_kgs`, `payment_method`, `date_created`, `date`) VALUES
 (4, 5, 4, 2, '2022-10-06 09:17:26', '2022-10-06'),
-(5, 4, 5, 1, '2022-10-05 22:32:30', '2022-10-05');
+(5, 4, 5, 1, '2022-10-05 22:32:30', '2022-10-05'),
+(6, 7, 10, 1, '2022-10-06 07:01:36', '2022-10-06'),
+(7, 11, 4, 2, '2022-10-10 13:31:55', '2022-10-10');
 
 -- --------------------------------------------------------
 
@@ -91,7 +104,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email_address`, `phone_number`, `password`, `user_level`, `date_created`) VALUES
 (1, 'Admin', 'DBAdmin', 'admin@gmail.com', '0746 055 476', 'admin.', 3, '2022-09-30 13:41:59'),
 (2, 'user', 'user_test', 'user@gmail.com', '0789 765 564', 'user.', 1, '2022-09-30 13:42:45'),
-(3, 'Aaqil', 'Dequantes', 'aaqil@gmail.com', '0746 055 487', 'aaqil.', 1, '2022-10-06 00:30:20');
+(3, 'Aaqil', 'Dequantes', 'aaqil@gmail.com', '0746 055 487', 'aaqil.', 2, '2022-10-06 00:30:20'),
+(4, 'Hellen', 'Wahome', 'hellen@gmail.com', '0787 654 321', 'hellen', 1, '2022-10-06 07:04:33');
 
 --
 -- Indexes for dumped tables
@@ -124,19 +138,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
